bc39fa774c8f0372b740feff687b26b07e14ce30
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-05-17 11:15:13 -0500

    commit tree 72440a3338e50b20c04d6969114e461fddba194a

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [doggy, setDoggy] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDoggy(data.message);
+      });
+  }, []);
+  
+  if (!doggy) return <p>Loading...</p>;
+
+  return <img src={doggy} alt="A Random Dog" />;
+}
+
+export default App;
